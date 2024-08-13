ba13763cf4dca15470fdf3bd1d2bd38ec846a7d7
ngocthuc2310 <107554605+ngocthuc2310@users.noreply.github.com>
2024-08-13 12:10:14 +0700

    msg

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const MONGOOSE =
 mongoose
   .connect(MONGOOSE)
   .then((result) => {
-    app.listen(5000, () => {
+    app.listen(5000, "127.0.0.1", () => {
       console.log("server running!");
     });
   })
